6297ad7c1fb061a092d26f10323f598499d704d6
Ma Jie Yue <majieyue@gmail.com>
2024-07-15 01:54:07 +0800

    add headers dependency

--- bind.bpf.c
+++ bind.bpf.c
@@ -1,19 +1,20 @@
 #include <linux/sched.h>
+#include <linux/ptrace.h>
 #include <linux/in.h>
-
-
 #include <linux/bpf.h>
+
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
+#include <bpf/bpf_tracing.h>
 
 
 SEC("kprobe/sys_bind")
 int bpf_prog(struct pt_regs *ctx) {
         struct sockaddr_in* addr = (struct sockaddr_in *)PT_REGS_PARM2(ctx);
 
-        if (ntohs(addr->sin_port) == 8080) {
+        if (bpf_ntohs(addr->sin_port) == 8080) {
                 bpf_printk("Access denied! pid: %d, port: %d\n",
-                        bpf_get_current_pid_tgid()>>32, ntohs(addr->sin_port));
+                        bpf_get_current_pid_tgid()>>32, bpf_ntohs(addr->sin_port));
 		return -1;
         }
 
