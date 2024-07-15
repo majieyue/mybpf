17d60a7aea0c5ab603bcb61d537cd31a8a5d813b
Ma Jie Yue <majieyue@gmail.com>
2024-07-15 11:31:15 +0800

    add bpf_probe_read_kernel in bpf

--- bind.bpf.c
+++ bind.bpf.c
@@ -8,13 +8,15 @@
 #include <bpf/bpf_tracing.h>
 
 
-SEC("kprobe/sys_bind")
+SEC("kprobe/__x64_sys_bind")
 int bpf_prog(struct pt_regs *ctx) {
-        struct sockaddr_in* addr = (struct sockaddr_in *)PT_REGS_PARM2(ctx);
+        struct sockaddr_in addr;
+	
+	bpf_probe_read_kernel(&addr, sizeof(addr), (void *)PT_REGS_PARM2(ctx));
 
-        if (bpf_ntohs(addr->sin_port) == 8080) {
+        if (bpf_ntohs(addr.sin_port) == 8080) {
                 bpf_printk("Access denied! pid: %d, port: %d\n",
-                        bpf_get_current_pid_tgid()>>32, bpf_ntohs(addr->sin_port));
+                        bpf_get_current_pid_tgid()>>32, bpf_ntohs(addr.sin_port));
 		return -1;
         }
 
